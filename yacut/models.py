bca55eca31bf6cd9644f1774e4e788021432b6e4
erges699 <erges699@gmail.com>
2026-06-23 15:56:07 +0300

    20260623-01

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,23 @@
 from datetime import datetime
+from flask import url_for
 
-from yacut import db
+from . import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String(256), nullable=False)
+    short = db.Column(db.String(16), unique=True, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return dict(
+            url=self.original,
+            short_link=url_for(
+                'short_view', short=self.short, _external=True
+            )
+        )
+
+    def from_dict(self, data):
+        self.original = data['url']
+        self.short = data['custom_id']
